779d36be73b3414a6e80595933ae6bf2f6cb0ba2
Golluméo <p.mauriello54@gmail.com>
2025-04-30 00:47:48 +0200

    test(Router): add initial RouterTest with route matching scenarios

---
+++ tests/Router/RouterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Feature: Route Matching', function () {
+    // TODO
+    it('can register a new route', function () {
+        expect(true)->toBeTrue();
+    });
+
+    it('can match a request to a route', function () {
+        expect(false)->toBeFalsy();
+    });
+});
