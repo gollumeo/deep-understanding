32101e0c623ba265ef584171aee96eb6ccaf183d
Golluméo <p.mauriello54@gmail.com>
2025-04-30 18:48:53 +0200

    test(Router): specify HTTP verb in route registration and matching tests

--- tests/Router/RouterTest.php
+++ tests/Router/RouterTest.php
@@ -6,7 +6,7 @@ use Router\Router;
 use Tests\Fixtures\DummyController;
 
 describe('Feature: Route Matching', function () {
-    it('can register a new route', function () {
+    it('can register a new `GET` route', function () {
         $router = new Router();
         $router->register(verb: 'GET', uri: '/users', action: [DummyController::class, 'index']);
         $routes = $router->list();
@@ -23,7 +23,7 @@ describe('Feature: Route Matching', function () {
         ]);
     });
 
-    it('can match a request to a route', function () {
+    it('can match a `GET` request to a route', function () {
         expect(false)->toBeFalsy();
     });
 });
