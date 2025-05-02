a800d5f526eb34aa1e7bb3c874447f8aafe3a28e
Golluméo <p.mauriello54@gmail.com>
2025-05-03 01:03:26 +0200

    refactor(DispatchActionTest): use RouteAction value object in DispatchAction instantiation

--- tests/Dispatching/DispatchActionTest.php
+++ tests/Dispatching/DispatchActionTest.php
@@ -3,11 +3,13 @@
 declare(strict_types=1);
 
 use Dispatching\DispatchAction;
+use Dispatching\ValueObject\RouteAction;
 use Tests\Fixtures\DummyController;
 
 describe('Feature: Route Action Dispatching', function () {
     it('must invoke the intercepted action', function () {
-        $dispatcher = new DispatchAction([DummyController::class, 'index']);
+        $routeAction = new RouteAction([DummyController::class, 'index']);
+        $dispatcher = new DispatchAction($routeAction);
         $result = $dispatcher->execute();
 
         expect($result)->toBe('hello-world');
